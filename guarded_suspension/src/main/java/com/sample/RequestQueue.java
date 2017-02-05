b7bbec4c726f31526caa07d2665cff18207a188a
Lunran <feitcounter@gmail.com>
2017-02-05 19:09:26 +0900

    used BlockingQueue for guarded_suspension pattern

--- guarded_suspension/src/main/java/com/sample/RequestQueue.java
+++ guarded_suspension/src/main/java/com/sample/RequestQueue.java
@@ -1,28 +1,28 @@
 package com.sample;
 
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class RequestQueue {
 
-	private final Queue<Request> queue = new LinkedList<Request>();
+	private final BlockingQueue<Request> queue = new LinkedBlockingQueue<Request>();
 
-	public synchronized Request getRequest() {
-		while (queue.peek() == null) {
-			try {
-				System.out.println("wait..");
-				wait();
-				System.out.println("wake!");
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+	public Request getRequest() {
+		Request req = null;
+		try {
+			req = queue.take();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
-		return queue.remove();
+		return req;
 	}
 
-	public synchronized void putRequest(Request request) {
-		queue.offer(request);
-		notifyAll();
+	public void putRequest(Request request) {
+		try {
+			queue.put(request);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
