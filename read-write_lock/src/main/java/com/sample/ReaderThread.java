7b1c88b66b533b4411bf5e373ed0492a6e621522
Lunran <feitcounter@gmail.com>
2017-02-07 19:56:07 +0900

    added read-write_lock pattern

---
+++ read-write_lock/src/main/java/com/sample/ReaderThread.java
@@ -0,0 +1,23 @@
+package com.sample;
+
+public class ReaderThread extends Thread {
+
+	private final Data data;
+
+	public ReaderThread(Data data) {
+		this.data = data;
+	}
+
+	public void run() {
+		try {
+			while (true) {
+				char[] readbuf = data.read();
+				System.out.println(Thread.currentThread().getName() + " reads " + String.valueOf(readbuf));
+			}
+		}
+		catch (InterruptedException ie) {
+			ie.printStackTrace();
+		}
+	}
+
+}
