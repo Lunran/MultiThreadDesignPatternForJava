f64bb21ba547b3453af8fd238e0cf98856ce2586
Lunran <feitcounter@gmail.com>
2017-02-07 12:31:00 +0900

    (producer_consumer) sends interrupt after 10 senconds

--- producer_consumer/src/main/java/com/sample/Main.java
+++ producer_consumer/src/main/java/com/sample/Main.java
@@ -1,14 +1,34 @@
 package com.sample;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 class Main {
 
 	public static void main(String args[]) {
 		Table table = new Table(3);
-		new MakerThread("MakerThread-1", table, 123).start();
-		new MakerThread("MakerThread-2", table, 456).start();
-		new MakerThread("MakerThread-3", table, 789).start();
-		new EaterThread("EaterThread-1", table, 012).start();
-		new EaterThread("EaterThread-2", table, 345).start();
-		new EaterThread("EaterThread-3", table, 678).start();
+		List<Thread> threads = new ArrayList<Thread>();
+		Collections.addAll(threads,
+							new MakerThread("MakerThread-1", table, 123),
+							new MakerThread("MakerThread-2", table, 456),
+							new MakerThread("MakerThread-3", table, 789),
+							new EaterThread("EaterThread-1", table, 012),
+							new EaterThread("EaterThread-2", table, 345),
+							new EaterThread("EaterThread-3", table, 678));
+
+		for (Thread thread: threads) {
+			thread.start();
+		}
+
+		try {
+			Thread.sleep(TimeUnit.SECONDS.toMillis(10));
+		} catch (InterruptedException e) {
+		}
+
+		for (Thread thread: threads) {
+			thread.interrupt();
+		}
 	}
 }
