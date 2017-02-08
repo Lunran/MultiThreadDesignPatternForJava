f7f5a2decb6cbd5eaaded36b9af1116aa9835633
Lunran <feitcounter@gmail.com>
2017-02-08 12:50:10 +0900

    (thread-per-message) used ExecutorService

--- thread-per-message/src/main/java/com/sample/Main.java
+++ thread-per-message/src/main/java/com/sample/Main.java
@@ -1,21 +1,23 @@
 package com.sample;
 
-import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 class Main {
 
 	public static void main(String args[]) {
 		System.out.println("main BEGIN");
-		Host host = new Host(new Executor() {
-				@Override
-				public void execute(Runnable r) {
-					new Thread(r).start();
-				}
-			});
-		host.request(10, 'A');
-		host.request(20, 'B');
-		host.request(30, 'C');
-		System.out.println("main END");
+		ExecutorService executorService = Executors.newCachedThreadPool();
+		Host host = new Host(executorService);
+		try {
+			host.request(10, 'A');
+			host.request(20, 'B');
+			host.request(30, 'C');
+		}
+		finally {
+			executorService.shutdown();
+			System.out.println("main END");
+		}
 	}
 
 }
